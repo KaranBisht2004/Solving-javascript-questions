3145e7a5d0a8a738213056aa0aed1024b9332a42
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:06:19 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\8-What are functions.js.Name

---
+++ 8-What are functions.js
@@ -0,0 +1,12 @@
+// what is a function in javascript?
+
+
+//function is a reusable block of code that performs a specific task. It takes inputs as parameters and gives an output using the return keyword.
+
+
+//JavaScript has different types of functions
+
+// -Function Declaration (Normal Function)
+// -Function Expression
+// -Arrow Function
+// -IIFE (Immediately Invoked Function Expression)"
