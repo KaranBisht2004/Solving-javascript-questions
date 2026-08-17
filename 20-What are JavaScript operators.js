c405eee0af50cef7f9c99865919c9f690bb8dfc5
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:05:42 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\20-What are JavaScript operators.js.Name

---
+++ 20-What are JavaScript operators.js
@@ -0,0 +1,10 @@
+// What are JavaScript operators?
+
+// Operators in JavaScript are special symbols or keywords used to perform operations on values and variables
+
+// 1- Arithmetic Operators: +, -, *, /, %, **
+// 2- Assignment Operators: =, +=, -=, /= 
+// 3- Comparison Operators: ==, ===, !=, !==, >, <, >=, <=
+// 4- Logical Operators: &&, ||, !
+// 5- Unary Operators: ++, --, typeof
+// 6- Ternary Operator: Short-hand if-else condition (condition ? expr1 : expr2)
