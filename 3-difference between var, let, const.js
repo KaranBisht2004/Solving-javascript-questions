84c02d4f9510c1265dbbaea17a032c41c3ed4654
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:06:00 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\3-difference between var, let, const.js.Name

---
+++ 3-difference between var, let, const.js
@@ -0,0 +1,9 @@
+// What is the difference between var, let, and const?
+ 
+
+
+//"var is function-scoped. It can be both reassigned and redeclared."
+
+//"let is block-scoped. It can be reassigned, but not redeclared in the same scope."
+
+//"const is block-scoped and used for constant values. It can neither be reassigned nor redeclared."
