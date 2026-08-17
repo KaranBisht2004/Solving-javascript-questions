244c08ac9a1ee4175d9c17f574edd61a84784a22
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:05:12 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\14-ternary operator.js.Name

---
+++ 14-ternary operator.js
@@ -0,0 +1,8 @@
+// What is a ternary operator?
+
+// The ternary operator is a conditional operator used as a shorthand for an if-else statement.
+
+let age = 50;
+let checking = age >= 18 ? 'You are an adult' : 'No, you are not an adult';
+console.log(checking);
+
