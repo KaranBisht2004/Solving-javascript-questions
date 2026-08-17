619545122949d729df0d02e8b305d6f1ae34cb97
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:06:04 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\4-data types .js.Name

---
+++ 4-data types .js
@@ -0,0 +1,22 @@
+// What are data types in JavaScript?
+
+
+// JavaScript has two main categories of data types:
+//  Primitive and Non-Primitive (Reference) types.
+
+// 1- Primitive Data Types include 
+// - String 
+// - Number
+// - Boolean
+// - Undefined
+// - Null
+// - BigInt
+// - Symbol
+//  They are immutable, stored directly in Stack memory
+
+// 2- Non-Primitive (Reference) Data Types include 
+// - Objects
+// - Arrays 
+// - Functions
+//  They are mutable, stored in Heap memory, 
+// they share the same memory location pointer
