3e6fd96261c6652a7190b7a81135748f36cf88e7
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:04:56 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\10- null & undefind.js.Name

---
+++ 10- null & undefind.js
@@ -0,0 +1,7 @@
+// What is the difference between null and undefined?
+
+// undefined and null both mean empty / no value 
+
+//undefined means a variable has been declared, but no value is assigned to it, so the JS engine automatically gives it undefined.
+
+//null is intentionally assigned by the developer
