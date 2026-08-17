804d0b51c8cf0e14e843753d1943b5904f3757a8
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:05:16 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\15-template literals.js.Name

---
+++ 15-template literals.js
@@ -0,0 +1,13 @@
+// What are template literals?
+
+//Template literals are an ES6 string feature wrapped in backticks (`). and embed variables using ${}"
+
+
+let name = "Karan";
+let role = "Developer";
+
+// Traditional way
+console.log("Hi, I am " + name + " and I am a " + role + ".");
+
+// Template Literal way
+console.log(`Hi, I am ${name} and I am a ${role}.`);
