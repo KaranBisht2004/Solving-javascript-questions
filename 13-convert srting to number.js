b44d85da80950ed73cd6fb60aad471ab600ee7fa
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:05:07 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\13-convert srting to number.js.Name

---
+++ 13-convert srting to number.js
@@ -0,0 +1,19 @@
+// How to convert a string to a number?
+
+// 1- use Number
+
+let a = '100'// this string 
+console.log(typeof(a));//string
+console.log(typeof(Number(a)));//number
+
+let b = Number(a)
+console.log(typeof(b));//number
+
+// 2- use +
+
+let c = '500';
+console.log(typeof(c));//string
+
+let d = +c
+console.log(typeof(d));//number
+ 
