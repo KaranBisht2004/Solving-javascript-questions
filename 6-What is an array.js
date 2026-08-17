3ccf9b98c6a3ca603a57449f55c263faec6d5309
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-17 22:06:12 +0530

    add C:\Users\vb041\OneDrive\Desktop\solving JS Qustions\6-What is an array.js.Name

---
+++ 6-What is an array.js
@@ -0,0 +1,3 @@
+// What is an array?
+
+// Array is an ordered and zero-indexed collection that stores multiple values under a single     variable. It is a non-primitive data type that is stored in Heap memory 
