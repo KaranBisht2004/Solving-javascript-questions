6a8c46ff9eb49013de92d0a13a35b64b057a2f8d
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-18 22:51:39 +0530

    25-What do you mean by B...

---
+++ 25-What do you mean by BOM.js
@@ -0,0 +1,3 @@
+// What do you mean by BOM?
+
+// BOM stands for Browser Object Model. It allows JavaScript to interact with the browser using features like alert(), confirm(), prompt(), and window.open().
