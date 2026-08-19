36987d04735d5458ea6b4701e0e6e340a505647e
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-19 23:07:01 +0530

    36-what is lexical scope.js

---
+++ 36-what is lexical scope.js
@@ -0,0 +1,4 @@
+//What is lexical scope?
+
+// Lexical Scope ka matlab hai ki variable ki accessibility is baat par depend karti hai ki wo code mein physically kahan declare hua hai, na ki wo kahan se call ho raha hai.
+
