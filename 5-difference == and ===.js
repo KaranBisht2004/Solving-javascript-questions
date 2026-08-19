645e6bf4dae4809df3ff067948adf707a5ef40c6
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-19 23:07:03 +0530

    5-difference == and ===.js

--- 5-difference == and ===.js
+++ 5-difference == and ===.js
@@ -1,6 +1,6 @@
 // What is the difference between == and ===?
 
 
-// == is Loose Equality. It compares only values by performing automatic type conversion (coercion) if the data types are different.
+// == is Loose Equality. It compares only values by performing automatic type conversion  if the data types are different.
 
 // === is Strict Equality. It compares both the values and their data types without any conversion."
