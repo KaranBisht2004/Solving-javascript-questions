59fc93d1300fb43f1f5664aa3dca38f989d4fdfa
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-19 23:07:02 +0530

    42-Memory leak.js

---
+++ 42-Memory leak.js
@@ -0,0 +1,5 @@
+// What is a memory leak in JS?
+
+
+// Memory Leak JavaScript mein tab hota hai jab code kisi unused ya unnecessary data ko memory mein hold karke rakhta hai, aur Garbage Collector use RAM se clear nahi kar pata. Isse application slow hoti hai aur end mein 'Heap Out of Memory' error deke app crash ho jati hai.
+
