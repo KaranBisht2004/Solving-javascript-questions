e5036e65aa4edccff64bdf7211507b3f783441bd
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-19 23:07:01 +0530

    38-Sync-Asyn.js

---
+++ 38-Sync-Asyn.js
@@ -0,0 +1,8 @@
+// What is the difference between synchronous and asynchronous?
+
+
+// Synchronous code executes line-by-line in sequential order.
+// Each operation waits for the previous one to finish before executing (Blocking Code).
+
+// Asynchronous code allows time-consuming tasks (like API calls or timers) to run in the background.
+// It executes non-blocking operations without stopping the execution of the main thread.
