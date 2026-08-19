30576d2aae08ecb2fc3caf0e553ac6e8b59c6e09
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-19 23:07:03 +0530

    44-event loop.js

---
+++ 44-event loop.js
@@ -0,0 +1,6 @@
+// What is the event loop in JavaScript?
+
+// Event Loop Call Stack ko monitor Jaise hi Call Stack khali hota hai, 
+// Event Loop vese si task(useCallback) queue ke task ko call stack mai bhj dta hai 
+
+
