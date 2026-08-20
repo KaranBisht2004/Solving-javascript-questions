ee89e1bcf54d635e0b3656fdef7cd29540e69c5e
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-20 23:28:54 +0530

    39-localStorage-sessionStorag.js

---
+++ 39-localStorage-sessionStorag.js
@@ -0,0 +1,5 @@
+// What is localStorage?
+
+//localStorage browser ke andar ek web storage mechanism hai jo key-value pairs ke form mein data ko user ke device par save karta hai.
+
+//Iska sabse bada feature ye hai ki page refresh karne ya browser close karne ke baad bhi data delete nahi hota (No expiration time).
