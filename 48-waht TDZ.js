f35d942755fd32844cdbee85bde114b4fbfe23f8
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-20 23:28:55 +0530

    48-waht TDZ.js

---
+++ 48-waht TDZ.js
@@ -0,0 +1,3 @@
+// what is TDZ
+
+// TDZ wo duration hai jab let ya const variable memory mein hoist ho chuka hota hai par initialize nahi hua hota. Is zone ke andar access karne par JS ReferenceError deta hai.
