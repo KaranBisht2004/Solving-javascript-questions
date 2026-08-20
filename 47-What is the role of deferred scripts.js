8b971819868f99be10c9d7807c6f72ea31d5a9fb
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-08-20 23:28:55 +0530

    47-What is the role of deferred scripts.js

---
+++ 47-What is the role of deferred scripts.js
@@ -0,0 +1,4 @@
+// What is the role of deferred scripts in JavaScript?
+
+//S file background mein HTML ke saath-saath download toh ho jati hai, lekin execute tabhi hoti hai jab poora HTML load ho jaye. Isse page fast load hota hai aur rendering block nahi hoti."
+
